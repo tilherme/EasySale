72ab4b4b755eb2d7997ef1eb187e9110ace09450
Daniel Rezende <daniel.zende@gmail.com>
2020-04-24 15:41:37 -0300

    Finalizing the sales screen

--- src/pages/Sales/index.js
+++ src/pages/Sales/index.js
@@ -18,7 +18,19 @@ export default function Sales() {
         <div className="details">
 
           <div className="inputs">
-            <Card title="Informações da Compra" styles="" />
+            <Card title="Informações da Compra">
+                <div className="input-group">
+                  <input type="text" placeholder="Cód.Vendedor" style={{ width: 120 }} />
+                  <input type="text" placeholder="Nome do vendedor ..." readOnly />
+                  <input type="text" placeholder="% Desc." style={{ width: 80 }} />
+                </div>
+                <div className="input-group">
+                  <input type="text" placeholder="Cód.Produto" style={{ width: 120, height: 30 }} />
+                  <input type="text" placeholder="Descrição do Produto" readOnly style={{ height: 30 }} />
+                  <input type="text" placeholder="Unidade" style={{ width: 80, height: 30 }} />
+                  <button className="add">Adicionar</button>
+                </div>
+            </Card>
           </div>
 
           <div className="info-sale">
